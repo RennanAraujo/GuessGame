536db7cca0ced77da06a1e1a674a8901fb3602fb
Rennan Araujo <rennansoarescosta@gmail.com>
2022-03-21 14:09:27 -0300

    Fixes post change in result message

--- GameLauncher.java
+++ GameLauncher.java
@@ -1,5 +1,5 @@
 public class GameLauncher {
-	public static void main (String [] args){
+	public static void main (String [] args) {
 		GuessGame game  = new GuessGame();
 		game.startGame();
 	}
--- Player.java
+++ Player.java
@@ -2,6 +2,6 @@ public class Player {
 	int number = 0;
 	public void guess(){
 		number = (int) (Math.random() * 10);
-		//System.out.println("Imma thinking in number " + number);
+		//System.out.println("Imma thinking about the number " + number);
 	}
 }
